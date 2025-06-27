e2385556cee12bd7c468f8043362dcca9a37b20c
MantokKa <imselfno@gmail.com>
2025-06-28 03:28:59 +0800

    .

--- components/products.vue
+++ components/products.vue
@@ -17,7 +17,7 @@
             :alt="product.imageAlt"
             class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
           />
-          <h3 class="mt-4 text-sm text-gray-700">{{ product.name }}</h3>
+          <h3 class="mt-4  text-lg text-gray-700">{{ product.name }}</h3>
           <p class="mt-1 text-lg font-medium text-gray-900">
             {{ product.price }}
           </p>
