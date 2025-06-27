5e4159fbc0b3e5d877b59f4891773e90413e03c0
MantokKa <imselfno@gmail.com>
2025-06-28 00:06:32 +0800

    product bg to wood-style

--- components/products.vue
+++ components/products.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white rounded-2xl">
+  <div class=" bg-amber-500 rounded-2xl">
     <div
       class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8"
     >
